70f32a425a3dd006129578d1bd4d525158857352
SilentRequiem <66715677+SilentRequiem@users.noreply.github.com>
2025-04-09 11:47:19 -0400

    start

---
+++ HU Project/HU Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HU Project
+//
+//  Created by Michael Amoo on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HU Project/HU Project/HU_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HU_ProjectApp.swift
+//  HU Project
+//
+//  Created by Michael Amoo on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HU_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
